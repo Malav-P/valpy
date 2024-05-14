3fe3cfd87852091fee9ca921d2c8cd2912b671fe
Malav_P <malavpatel2022@u.northwestern.edu>
2024-05-14 14:35:50 -0400

    restructure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='valpy',
+      version='1.0.0',
+      packages=find_packages(),
+      install_requires = ['numpy',
+                          'rudolfpy @ git+https://github.gatech.edu/SSOG/python-filter.git@dev'],
+    )
