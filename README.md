6e0e47736302ceddc2583386b9e18a2d3c5468d3
Malav_P <malavpatel2022@u.northwestern.edu>
2024-05-15 13:42:44 -0400

    PAT to setup.py file

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Installation
 
+# From Source
 ```bash
 (venv)$ git clone https://github.gatech.edu/mpatel636/SSA_Validation.git  # clone the source code
 
@@ -7,6 +8,10 @@
 
 (venv)$ pip install . # install into virtual environment using pip
 
+```
+# Conda
+```bash
+conda install malavp::valpy
 ```
 ## Example
 See the [example](./example/test.ipynb) file.
--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='valpy',
       version='1.0.0',
       packages=find_packages(),
       install_requires = ['numpy',
-                          'rudolfpy @ git+https://github.gatech.edu/SSOG/python-filter.git@dev'],
+                          'rudolfpy @ git+https://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@github.gatech.edu/SSOG/python-filter.git@dev'],
     )
